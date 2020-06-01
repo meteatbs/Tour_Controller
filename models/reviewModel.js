efdab3c65bcdc19ba4a87702c3e2b39c8c3a7068
meteatbs <130202020@kocaeli.edu.tr>
2020-06-01 13:26:27 +0300

    Initial Commit

---
+++ models/reviewModel.js
@@ -0,0 +1,66 @@
+//review/rating/createdAt/reference to tour/ref to user
+const mongoose = require('mongoose');
+
+
+const reviewSchema=new mongoose.Schema({
+
+    review:{
+        type:String,
+        required: [true, 'Please Add a review']
+    },
+    rating:{
+        type:Number,
+        required: [true, 'A review must have a rate'],
+        min:1,
+        max:5
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+        
+    },
+    tour:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Tour',
+        required:[true,'Review must belog to a tour.']
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:'User',
+        required:[true,'review must belong to user']
+    }
+
+
+},
+{
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+
+
+reviewSchema.pre(/^find/,function(next){
+    // this.populate({
+      
+    //   path:'tour',
+    //   select:'name'
+
+    // }).populate({
+    //     path:'user',
+    //     select:'name photo'
+    // })
+    this.populate({
+          path:'user',
+          select:'name photo'
+      });
+
+    next();
+});
+
+
+const Review=mongoose.model('Review',reviewSchema);
+
+module.exports=Review;
+
+
---
+++ routes/reviewRoutes.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const reviewController=require('./../controllers/reviewController');
+const authController=require('./../controllers/authController');
+
+const router = express.Router({ mergeParams:true });
+
+  //POST /tour/234fad4/reviews birleştirir
+    //GET /tour/234fad4/reviews
+  //Post /reviews
+
+
+router.use(authController.protect);
+
+
+router.route
+
+
+router
+  .route('/').get(reviewController.getAllReviews).post(authController.restrictTo('user'),reviewController.setTourUserIds, reviewController.createReview);
+
+
+  router.route('/:id').get(reviewController.getReview).patch(authController.restrictTo('user','admin'),reviewController.updateReview).delete(authController.restrictTo('user','admin'),reviewController.deleteReview);
+
+  module.exports = router;
